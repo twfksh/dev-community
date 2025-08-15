686d662fa6b89aa33ca428b41998b1f99aa4907a
Toufiq Shishir <toufiq_a24@outlook.com>
2025-08-15 12:27:16 +0600

    feat: add crud operations to Posts service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post } from './schemas/posts.schema';
 import { Model } from 'mongoose';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -9,8 +10,26 @@ export class PostsService {
     @InjectModel(Post.name) private readonly postModel: Model<Post>,
   ) {}
 
-  async create(post: Post): Promise<Post> {
+  async create(post: CreatePostDto): Promise<Post> {
     const createdPost = new this.postModel(post);
     return await createdPost.save();
+  }
+
+  async findAll(): Promise<Post[]> {
+    return await this.postModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Post | null> {
+    return await this.postModel.findById(id).exec();
+  }
+
+  async update(id: string, post: CreatePostDto): Promise<Post | null> {
+    return await this.postModel
+      .findByIdAndUpdate(id, post, { new: true })
+      .exec();
+  }
+
+  async remove(id: string): Promise<Post | null> {
+    return await this.postModel.findByIdAndDelete(id).exec();
   }
 }
