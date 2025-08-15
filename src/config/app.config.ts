0b58ed90aabbf937c51b6f9fd4ce6a4018978837
Toufiq Shishir <toufiq_a24@outlook.com>
2025-08-15 11:13:07 +0600

    feat: add config schema with validation for environment variables

---
+++ src/config/app.config.ts
@@ -0,0 +1,17 @@
+export default () => ({
+  node_env: process.env.NODE_ENV ?? 'development',
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  dbUri:
+    process.env.MONGO_URI ||
+    (() => {
+      throw new Error('MONGO_URI not set');
+    })(),
+  jwt: {
+    secret:
+      process.env.JWT_SECRET ||
+      (() => {
+        throw new Error('JWT_SECRET not set');
+      })(),
+    expiresIn: process.env.JWT_EXPIRES_IN ?? '1h',
+  },
+});
