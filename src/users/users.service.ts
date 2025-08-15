5b207874434b9c57d148818ea68e3f59ceb1ff0c
Toufiq Shishir <toufiq_a24@outlook.com>
2025-08-15 11:37:47 +0600

    feat: Implement CRUD operations for users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { User } from './schemas/users.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersModel: Model<User>) {}
+
+  async create(user: User): Promise<User> {
+    const createdUser = new this.usersModel(user);
+    return await createdUser.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.usersModel.find().exec();
+  }
+
+  async findOne(sub: string): Promise<User | null> {
+    return await this.usersModel
+      .findOne({ $or: [{ id: sub }, { email: sub }] })
+      .exec();
+  }
+
+  async update(id: string, user: User): Promise<User | null> {
+    return await this.usersModel
+      .findByIdAndUpdate(id, user, { new: true })
+      .exec();
+  }
+
+  async remove(id: string): Promise<User | null> {
+    return await this.usersModel.findByIdAndDelete(id).exec();
+  }
+}
