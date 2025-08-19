eb5dff0d3c81e42633594c746ab077023a531163
Toufiq Shishir <toufiq_a24@outlook.com>
2025-08-19 11:34:03 +0600

    Add logger middleware for HTTP request logging

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('finish', () => {
+      this.logger.log(
+        `Method: ${method}, URL: ${originalUrl}, User-Agent: ${userAgent} from IP: ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
