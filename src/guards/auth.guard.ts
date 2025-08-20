19a590750f7db9c3bde64e146b21ad3c1078011f
Toufiq Shishir <toufiq_a24@outlook.com>
2025-08-20 19:03:25 +0600

    Refactor AuthGuard to utilize cache for user roles; add CacheInterceptor to PostsController

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -10,13 +11,15 @@ import { AuthPayload } from '../types/auth-payload';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
 import extractTokenFromHeader from '../helpers/auth.helper';
+import type { Cache } from 'cache-manager';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private reflector: Reflector,
-  ) {}
+    @Inject('CACHE_MANAGER') private cacheManager: Cache,
+  ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
@@ -35,6 +38,12 @@ export class AuthGuard implements CanActivate {
       );
     }
 
+    const cachedRoles = await this.cacheManager.get<string[]>(`user_roles_${req['user'].sub}`);
+    if (cachedRoles) {
+      req['user'].roles = cachedRoles;
+      return true;
+    }
+
     const payload = await this.jwtService
       .verifyAsync<AuthPayload>(token)
       .catch(() => {
@@ -42,6 +51,8 @@ export class AuthGuard implements CanActivate {
           'Invalid credentials. Token is invalid or expired.',
         );
       });
+
+    await this.cacheManager.set(`user_roles_${req['user'].sub}`, payload.role);
 
     req['user'] = payload;
 
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,17 +7,20 @@ import {
   Patch,
   Post,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { PostsService } from './posts.service';
 import { RolesGuard } from 'src/guards/roles.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
+@UseInterceptors(CacheInterceptor)
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('api/posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) { }
 
   @Post()
   createPost(@User('sub') userId: string, @Body() postDto: CreatePostDto) {
